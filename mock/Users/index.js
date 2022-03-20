10af0c64038637cf0a6114dc6e8d3073c4a679f8
Lucas Lima <lucalmendes9@gmail.com>
2022-03-20 20:13:09 -0300

    feat: mock data

---
+++ mock/Users/index.js
@@ -0,0 +1,32 @@
+const users = [
+    {
+        id: 98797,
+        name: 'Lucas Lima',
+        email: 'lucas@test.com',
+        phone: '(11) 93498-6536',
+        type: 'Admin'
+    },
+    {
+        id: 64434,
+        name: 'Jose Silva',
+        email: 'jose@test.com',
+        phone: '(22) 77654-8445',
+        type: 'Colab'
+    },
+    {
+        id: 42346,
+        name: 'Barbara Souza',
+        email: 'barbara@test.com',
+        phone: '(88) 54578-6754',
+        type: 'User'
+    },
+    {
+        id: 234356,
+        name: 'Jorge Vagner',
+        email: 'jorge@test.com',
+        phone: '(44) 54645-8556',
+        type: 'Espec'
+    }
+];
+
+export default users;
