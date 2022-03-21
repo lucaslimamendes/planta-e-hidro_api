7bc44c484f17e527cc86e4300bf8893428e644dc
Camaraz <cfael.21@gmail.com>
2022-03-21 17:48:48 -0300

    Login e correcoes

--- mock/Users/index.js
+++ mock/Users/index.js
@@ -1,6 +1,8 @@
 const users = [
     {
         id: 98797,
+        username: 'llima',
+        password: '1234',
         name: 'Lucas Lima',
         email: 'lucas@test.com',
         phone: '(11) 93498-6536',
@@ -8,6 +10,8 @@ const users = [
     },
     {
         id: 64434,
+        username: 'jsilva',
+        password: 'pegasus21',
         name: 'Jose Silva',
         email: 'jose@test.com',
         phone: '(22) 77654-8445',
@@ -15,6 +19,8 @@ const users = [
     },
     {
         id: 42346,
+        username: 'bsouza',
+        password: 'patoDonald10',
         name: 'Barbara Souza',
         email: 'barbara@test.com',
         phone: '(88) 54578-6754',
@@ -22,6 +28,8 @@ const users = [
     },
     {
         id: 234356,
+        username: 'jvagner',
+        password: 'senhaReserva123',
         name: 'Jorge Vagner',
         email: 'jorge@test.com',
         phone: '(44) 54645-8556',
--- src/app/controllers/UserController/create.js
+++ src/app/controllers/UserController/create.js
@@ -1,18 +1,22 @@
 import bcrypt from 'bcryptjs';
+import users from '../../../../mock/Users';
 
 export default async (req, res) => {
-    const { name, email, phone, password } = req.body;
+    const { name, email, phone, password, username } = req.body;
     
-    if(!name || !email || !phone || !password)
+    if(!name || !email || !phone || !password || !username)
         return res.status(400).json({ error: 'Missing required fields!' });
 
     const user = {
         id: Math.floor(Math.random() * 500) + 1,
-        name,
+        username,
         password: await bcrypt.hash(password, 10),
+        name,
         email,
         phone,
         type: 'Colab'
     };
+
+    user.push(users);
     return res.status(201).json(user);
 };
