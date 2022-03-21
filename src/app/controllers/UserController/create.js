b29620e8836de3bd57cfcefb75290a3630bdfa0a
Lucas Lima <lucalmendes9@gmail.com>
2022-03-20 21:46:58 -0300

    feat: user password

--- src/app/controllers/UserController/create.js
+++ src/app/controllers/UserController/create.js
@@ -1,12 +1,15 @@
+import bcrypt from 'bcryptjs';
+
 export default async (req, res) => {
-    const { name, email, phone } = req.body;
+    const { name, email, phone, password } = req.body;
     
-    if(!name || !email || !phone)
+    if(!name || !email || !phone || !password)
         return res.status(400).json({ error: 'Missing required fields!' });
 
     const user = {
         id: Math.floor(Math.random() * 500) + 1,
         name,
+        password: await bcrypt.hash(password, 10),
         email,
         phone,
         type: 'Colab'
--- src/app/controllers/UserController/update.js
+++ src/app/controllers/UserController/update.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import users from '../../../../mock/Users';
 
 export default async (req, res) => {
@@ -5,11 +6,14 @@ export default async (req, res) => {
     if(!user)
         return res.status(404).json({ error: 'User not found!' });
 
-    const { name, email, phone } = req.body;
+    const { name, email, phone, password } = req.body;
 
     user.name = name ? name : user.name;
     user.email = email ? email : user.email;
     user.phone = phone ? phone : user.phone;
+
+    if(password)
+        user.password = await bcrypt.hash(password, 10);
 
     return res.json(user);
 };
