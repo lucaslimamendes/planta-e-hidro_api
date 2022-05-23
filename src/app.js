c1d053921aaa153a30e6f405daf407172e510319
Lucas Lima <lucalmendes9@gmail.com>
2022-05-23 12:54:58 -0300

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import './config/database';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
