95f2ca7d1a3f41e01b77398e481cc914cd7dcdc8
Lucas Lima <lucalmendes9@gmail.com>
2022-05-23 04:04:52 -0300

    feat: docs swagger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import routes from './routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../swagger-output.json';
 import './config/database';
 
 const app = express();
@@ -7,5 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 export default app;
